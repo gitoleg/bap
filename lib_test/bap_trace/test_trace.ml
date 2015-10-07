5c789f93c705e8d898343f3b117fbd22e7282715
Oleg <forown@yandex.ru>
2015-10-07 15:27:01 +0300

    minor

--- lib_test/bap_trace/test_trace.ml
+++ lib_test/bap_trace/test_trace.ml
@@ -9,19 +9,19 @@ open Trace.Meta
  
 let () = Bap_trace_binprot.register ()
 
-let move = Trace.Move.({cell = Word.b1; data = Word.b1;})
-let var = Var.create ~tmp:true "temp" (Type.Imm 0x1) 
-let reg = Trace.Move.({cell = var; data = Word.b1;})
+let bed = Word.of_int32 0xBEDl
+let coffee = Word.of_int32 0xC0FFEEl
+let move = Trace.Move.({cell = bed; data = coffee;})
+let var = Var.create ~tmp:true "temp" (Type.Imm 0xC0FFEE) 
+let reg = Trace.Move.({cell = var; data = coffee;})
 let bin = Trace.Binary.({path="/dev/null"; arch=`x86; stripped = None})
 let trc = Trace.Tracer.({name="test_tracer"; args=Array.empty (); version="";}) 
 
 let test_events = 
-  let c = Value.create in
-  [ c memory_load move;
-    c memory_store move;
-    c register_read reg;
-    c register_write reg;
-    c pc_update Word.b1; ]
+  [ Value.create memory_load move;
+    Value.create memory_store move;
+    Value.create register_read reg;
+    Value.create register_write reg; ]
 
 let test_meta = 
   let open Dict in
@@ -38,8 +38,13 @@ end
 let test_tool = Trace.register_tool (module Tool)
 let empty = Trace.create test_tool
 
-let seq_eql s s' = Seq.compare Value.compare s s' = 0
-let dict_eql d d' = Value.Dict.compare d d' = 0 
+let assert_seq_equal ~ctxt  =
+  let cmp s s' = Seq.compare Value.compare s s' = 0 in
+  assert_equal ~ctxt ~cmp 
+
+let assert_dict_equal ~ctxt = 
+  let cmp d d' = Value.Dict.compare d d' = 0 in
+  assert_equal ~ctxt ~cmp
 
 let save_and_load ctxt =
   let uri = Uri.of_string "file:///tmp/bap_trace.test" in
@@ -47,15 +52,15 @@ let save_and_load ctxt =
     let t = Trace.append empty (Seq.of_list test_events) in
     let t = Trace.set_meta t test_meta in
     let r = Trace.save uri t in
-    assert_equal ~ctxt (Ok ()) r in
+    assert_bool "save failed" (Result.is_ok r) in
   let load () = 
     match Trace.load uri with
     | Ok t -> 
       let evs = Trace.events t in
       let meta = Trace.meta t in
       let tool = Trace.tool t in
-      assert_equal ~ctxt ~cmp:seq_eql evs (Seq.of_list test_events);
-      assert_equal ~ctxt ~cmp:dict_eql meta test_meta;
+      assert_seq_equal ~ctxt evs (Seq.of_list test_events);
+      assert_dict_equal ~ctxt meta test_meta;
       assert_equal ~ctxt test_tool tool
     | Error s -> assert_failure "load failed" in
   save ();
@@ -66,14 +71,14 @@ let id ctxt =
   let t' = Trace.create test_tool in
   let id,id' = Trace.(id t, id t') in
   let not_eql = not (Trace.Id.equal id id') in
-  assert_bool "is mustn't be equal, failed" not_eql
+  assert_bool "id mustn't be equal, failed" not_eql
 
 let trace_tool ctxt = 
   let t = Trace.create test_tool in
   let tool = Trace.tool t in
   assert_equal ~ctxt tool test_tool
 
-let set_attr t attr a ctxt = 
+let set_and_get_attr t attr a ctxt = 
   let t = Trace.set_attr t attr a in
   match Trace.get_attr t attr with
   | None -> assert_failure "get attribute failed"
@@ -88,7 +93,7 @@ let set_meta t ctxt =
   let open Value.Dict in 
   let t = Trace.set_meta t test_meta in
   let meta = Trace.meta t in 
-  assert_equal ~ctxt ~cmp:dict_eql meta test_meta
+  assert_dict_equal ~ctxt meta test_meta
 
 let add_event t tag v ctxt = 
   let t = Trace.add_event t tag v in
@@ -96,27 +101,35 @@ let add_event t tag v ctxt =
   | Some s -> assert_bool "add event failed" s
   | None -> assert_failure "add event failed"
 
+let events ctxt = 
+  let evs = Seq.of_list test_events in
+  let t = Trace.append empty evs in
+  let evs' = Trace.events t in
+  assert_seq_equal ~ctxt evs evs'
+
 let find t tag v ctxt = 
   let t = Trace.add_event t tag v in
   match Trace.find t tag with 
   | None -> assert_failure "find failed" 
   | Some v' -> assert_equal ~ctxt v v'
 
-let find_all t count tag v ctxt = 
+let find_all count tag value ctxt = 
   let rec add n t = 
     if n < count then
-      Trace.add_event t tag v |> add (n + 1)
+      Trace.add_event t tag value |> add (n + 1)
     else t in
-  let t = add 0 t in
+  let t = add 0 empty in
   let s = Trace.find_all t tag in
-  let s = Seq.filter s ~f:(fun v' -> v = v') in
-  let len = Seq.length s in
-  assert_equal ~ctxt len count
+  let s = Seq.filter s ~f:(fun v -> v = value) in
+  assert_equal ~ctxt (Seq.length s) count
+
+let add_list t evs = 
+  List.fold_left evs ~init:empty 
+    ~f:(fun t (tag,v) -> Trace.add_event t tag v)
 
 let find_all_matching ctxt = 
-  let t = empty in
-  let t = Trace.add_event t memory_load move in
-  let t = Trace.add_event t memory_store move in
+  let memory_events = [memory_load,move; memory_store,move] in
+  let t = add_list empty memory_events in
   let t = Trace.add_event t register_read reg in
   let t = Trace.add_event t register_write reg in
   let s = Trace.find_all_matching t
@@ -125,16 +138,13 @@ let find_all_matching ctxt =
               case memory_store (fun x  -> `Memory) @@
               default           (fun () -> `Unknown)
             end) in
-  let is_memory p = p = `Memory in
-  let s' = Seq.filter s ~f:is_memory in
+  let s' = Seq.filter s ~f:(fun x -> x = `Memory) in
   assert_bool "failed find_all_matching"
-    (Seq.length s = Seq.length (Trace.events t) &&
-     Seq.length s' = 2)
+    (Seq.length s' = List.length memory_events)
 
 let fold_matching ctxt = 
-  let t = empty in
-  let t = Trace.add_event t memory_load move in
-  let t = Trace.add_event t memory_store move in
+  let memory_events = [memory_load,move; memory_store,move] in
+  let t = add_list empty memory_events in
   let t = Trace.add_event t register_read reg in
   let t = Trace.add_event t register_write reg in
   let s = Trace.fold_matching t ~init:[] ~f:(fun xs x -> xs @ x)
@@ -145,7 +155,8 @@ let fold_matching ctxt =
             end) in
   let is_memory (p,_) = p = `Memory in
   assert_bool "failed fold_matching"
-    (List.for_all s ~f:is_memory && List.length s = 2)
+    (List.for_all s ~f:is_memory && 
+     List.length s = List.length memory_events)
 
 let supports ctxt = 
   let m = Trace.supports empty memory_load in
@@ -154,21 +165,21 @@ let supports ctxt =
   assert_bool "supports failed" (m && m' && r)
 
 let contains t ctxt = 
-  let t = Trace.add_event t pc_update Word.b1 in
-  match Trace.contains t pc_update with
+  let t = Trace.add_event t memory_load move in
+  match Trace.contains t memory_load with
   | Some r -> assert_bool "contains failed" r
   | None -> assert_failure "contains failed"
 
 let append ctxt =
   let t = empty in
-  let c = Value.create in
   let t = Trace.add_event t register_read reg in
   let t = Trace.add_event t register_write reg in
+  let c = Value.create in
   let evs = [c register_read reg; c register_write reg] in
   let expected = Seq.of_list (evs @ test_events) in
   let t = Trace.append t (Seq.of_list test_events) in
   let events = Trace.events t in
-  assert_equal ~ctxt ~cmp:seq_eql events expected
+  assert_seq_equal ~ctxt events expected
 
 let next () = 
     let a = ref test_events in
@@ -180,14 +191,14 @@ let unfold ctxt =
   let t = Trace.unfold test_tool ~f:(next ()) ~init:() in
   let evs = Trace.events t in
   let evs' = Seq.of_list test_events in
-  assert_equal ~ctxt ~cmp:seq_eql evs evs'
+  assert_seq_equal ~ctxt evs evs'
 
-let check_calls ctxt = 
+let check_calls_order ctxt = 
   let t = Trace.unfold test_tool ~f:(next ()) ~init:() in
   let evs = Trace.events t in
   let _ = Trace.find t register_read in
   let evs' = Trace.events t in
-  assert_equal ~ctxt ~cmp:seq_eql evs evs'
+  assert_seq_equal ~ctxt evs evs'
 
 let memoize ctxt = 
   let a = ref 0 in
@@ -208,18 +219,19 @@ let suite =
     "save_and_load"     >:: save_and_load;
     "id"                >:: id;
     "tool"              >:: trace_tool;
-    "set_attr"          >:: set_attr empty binary bin;
+    "set_and_get_attr"  >:: set_and_get_attr empty binary bin;
     "has_attr"          >:: has_attr empty binary bin;
     "set_meta"          >:: set_meta empty;
     "add_event"         >:: add_event empty memory_load move;
+    "events"            >:: events;
     "find"              >:: find empty memory_load move;
-    "find_all"          >:: find_all empty 5 memory_load move;
-    "find_all_matching" >:: find_all_matching ;
+    "find_all"          >:: find_all 5 memory_load move;
+    "find_all_matching" >:: find_all_matching;
     "fold_matching"     >:: fold_matching;
-    "supports"          >:: supports;
     "contains"          >:: contains empty;
+    "supports"          >:: supports;
     "append"            >:: append;
     "unfold"            >:: unfold;
-    "check_calls"       >:: check_calls;
+    "check_calls"       >:: check_calls_order;
     "memoize"           >:: memoize;
   ]
