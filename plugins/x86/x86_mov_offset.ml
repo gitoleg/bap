ac3268d2dae4a40a8e921fae52fe4d2794873558
Oleg <forown@yandex.ru>
2017-01-12 15:48:24 -0500

    test string version of llvm 3.4

--- plugins/x86/x86_mov_offset.ml
+++ plugins/x86/x86_mov_offset.ml
@@ -195,5 +195,6 @@ module T_34 = Make(Ver_34)
 module T_38 = Make(Ver_38)
 
 let () =
-  if Llvm_disasm.version = "3.4" then T_34.register ()
+  printf "llvm version is %s\n "X86_llvm_config.llvm_version;
+  if X86_llvm_config.llvm_version = "3.4" then T_34.register ()
   else T_38.register ()
