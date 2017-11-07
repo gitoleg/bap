cf68fedff6ca9f9f1af15ee004e1bbdda25569e7
Oleg <forown@yandex.ru>
2017-11-07 15:48:16 -0500

    minor

--- .travis-veri.sh
+++ .travis-veri.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 # TODO add a BAP/bap-veri repository BinaryAnalysisPlatform
-git clone https://github.com/gitoleg/bap-veri.git
-opam pin add bap-veri bap-veri/ -n
-opam install -v bap-veri
-
-make veri
+if [ "$BAP_RUN_VERI" = true ]; then
+    git clone https://github.com/gitoleg/bap-veri.git
+    opam pin add bap-veri bap-veri/ -n
+    opam install -v bap-veri
+    make veri
+fi
