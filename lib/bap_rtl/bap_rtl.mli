7873a4e5d9819df0e947971c2f0e848db8702086
Oleg <forown@yandex.ru>
2018-04-19 14:59:58 -0400

    just typo

--- lib/bap_rtl/bap_rtl.mli
+++ lib/bap_rtl/bap_rtl.mli
@@ -17,7 +17,7 @@
     abstractions that adds brevity and therefore simplicity to user
     code.
 
-    Bap_rtl is a foundations, where one can build a lifter.
+    Bap_rtl is a foundation, where one can build a lifter.
     It consists from the next modules:
     - RTL
     - Bitwidth
@@ -330,7 +330,7 @@
     To be more concrete let's create an artificial example.
    {[
      1 let sort_of_add cpu ops =
-         2   let rt = unsigned cpu.reg ops.(0) in
+     2   let rt = unsigned cpu.reg ops.(0) in
      3   let ra = signed cpu.reg ops.(1) in
      4   let im = unsigned imm ops.(2) in
      5   let rc = signed cpu.reg ops.(3) in
