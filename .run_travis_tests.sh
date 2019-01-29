f8e334c90a1d63d0733c7b012ba84b8bf7e271ed
Oleg <forown@yandex.ru>
2019-01-29 12:52:45 -0500

    minor

--- .run_travis_tests.sh
+++ .run_travis_tests.sh
@@ -1,23 +1,14 @@
 TASK=$1
 
-#eval `opam config env`
-
 comp=4.05.0
 
 mkdir -p $HOME/.opam/$comp/
-
-ls -l $HOME
-ls -l $HOME/save_opam/
-ls -l $HOME/save_opam/bin/*
 
 cp -r $HOME/save_opam/bin/ $HOME/.opam/$comp/
 cp -r $HOME/save_opam/share/ $HOME/.opam/$comp/
 cp -r $HOME/save_opam/lib/ $HOME/.opam/$comp/
 
 export PATH=$HOME/.opam/$comp/bin:$PATH
-
-
-ls -l ~/.opam/$comp/lib/bap
 
 bap --version
 
@@ -31,6 +22,5 @@ if [ "$TASK" == "unit_tests" ]; then
 fi
 
 if [ "$TASK" == "veri" ]; then
-    bap-veri --help
     bash -exc 'make veri'
 fi
